dfdd4bd6e58ce24c0fa4219f980a41285b236748
TheAsdas <therealasdas@protonmail.com>
2020-11-09 19:49:38 -0300

    Pantalla de personas finalizada.

--- app/src/main/java/cl/example/comisariaVirtual/MainActivity.kt
+++ app/src/main/java/cl/example/comisariaVirtual/MainActivity.kt
@@ -1,8 +1,10 @@
 package cl.example.comisariaVirtual
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.view.View
 import android.webkit.*
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -10,9 +12,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-        val prefs = PreferenceManager.getDefaultSharedPreferences(this);
+    fun gotoPersonas(view: View)
+    {
+        val intent = Intent(
+            this,
+            EditorPersonas::class.java
+        )
 
+        startActivity(intent);
     }
 
 
